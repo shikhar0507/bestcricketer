be463a5c27373f2c1f95f4f238f74a3299513d50
shikhar0507 <kap.shikhar@gmail.com>
2018-02-15 22:19:05 +0530

    first graph completed

--- controllers/home.js
+++ controllers/home.js
@@ -4,48 +4,90 @@ var vm = this;
 
 
 vm.rawData = service.getData().then(function(res){
+  
+  vm.data = helper.parse(res,"*");
+  
 
-vm.data = helper.parse(res,"*");
+  
+  $scope.totalRuns = helper.score(vm.data,50).totalRuns();
+  $scope.totalCenturies = helper.score(vm.data,50).totalCenturies();
+  
+  vm.addDat = helper.sortByOpposition(vm.data);
+  console.log(vm.addDat);
 
 
+var countryName =[];
+var score = [];
+Object.keys(vm.addDat).forEach(function(c){
+  
+  score.push(vm.addDat[c]);
+})
+var sortedCountry = Object.keys(vm.addDat).sort(function(a,b){
+return vm.addDat[a] - vm.addDat[b];
+})
 
-$scope.totalRuns = helper.score(vm.data,50).totalRuns();
-$scope.totalCenturies = helper.score(vm.data,50).totalCenturies();
+
 
 
 // $scope.tempHolder = helper.sortByOpposition(vm.data);
 $scope.tempHolder = [{
-	key: 'Data',
-    values: [{
-      x: "pakistan",
-      y: 5
-    },{
-    	x:"america",
-    	y:10
-    }]
-  }];
+  key: 'Data',
+  values: [{
+    x: 0,
+    y: 0
+  },{
+    x:1,
+    y:1
+  },{
+    x:2,
+    y:2
+  },{
+    x:3,
+    y:3
+  },{
+    x:4,
+    y:4
+  },{
+    x:5,
+    y:5
+  },{
+    x:6,
+    y:6
+  },{
+    x:7,
+    y:7
+  }]
+}];
 
+$scope.cdata = sortedCountry;
+
+score.sort(function(a,b){
+  return a-b;
+})
+
+$scope.vsScore = score;
 
 })
 
+
 setTimeout(function (){
-
-
-
-
+  
+  
+  
+  
 },2000)
 
 // function modifyData(){
+  
+  
+  
+  // }
+  
+  
+  
 
-
-
-// }
-
-
-
-
-
-
+  
+  
 });
 
 
--- directives/lineChart.js
+++ directives/lineChart.js
@@ -5,42 +5,55 @@ return {
 	restrict :'E',
 	scope : {
 		data:'=',
+		country:'=',
+		score:'=',
 		height:'@' ,
 		width:'@'
+
 	},
 	template:'<svg ng-attr-height="{{height}}" ng-attr-width="{{width}}"></svg>',
-
+	
 	link : function(scope,element) {
 		var svg = element.find('svg');
+	
+		
+		
+		
+		
+	
+		
 		var chart;
-
+		
 		var update = function() {
 			d3.select(svg[0]).datum(scope.data).call(chart);
 		}
-
+		
 		scope.$watch(function() {
 			return angular.toJson(scope.data);
 		},function() {
-				if(chart) {
-					update();
-				}
+			if(chart) {
+				update();
 			}
-		);
-
-		scope.$on('lineChartLoad',update);
-
-		nv.addGraph(function(){
-			chart = nv.models.lineChart().showLegend(false).showYAxis(true).showXAxis(true);
-
-			chart.xAxis.axisLabel('x').tickFormat(d3.format('.2f'));
-			chart.yAxis.axisLabel('y').tickFormat(d3.format('.2f'));
-
+		}
+	);
+	
+	scope.$on('lineChartLoad',update);
+	nv.addGraph(function(){
+		setTimeout(function(){
+		chart = nv.models.discreteBarChart().showValues(false).staggerLabels(true)  
+			
+			chart.xAxis.tickValues([0,1,2,3,4,5,6,7]).tickFormat(function(d){return scope.country[d]})
+			
+			chart.yAxis.tickValues([0,1,2,3,4,5,6,7]).tickFormat(function (d) { return scope.score[d]});
+			
 			scope.$emit('lineChartLoad');
 			return chart;
-		})
-
-
-
-	}
+		},1000);	
+	})
+	
+	
+	
+	
+}
 }
 }]);

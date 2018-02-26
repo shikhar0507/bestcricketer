6b4f8d0e7ec9f1849e24c032f0b3e425a351cc69
shikhar <kap.shikhar@gmail.com>
2018-02-26 21:33:34 +0530

    change

--- controllers/home.js
+++ controllers/home.js
@@ -346,7 +346,7 @@ app.controller('home', function ($scope, $location, $http, helper, service, char
     setTimeout(function () {
       
       performanceInUserState();
-    }, 4000);
+    }, 2000);
   
   
   
